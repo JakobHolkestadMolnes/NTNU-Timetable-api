20e5da1a3c18efd4a014c8c291c7950e951de621
Jakob Holkestad Molnes <Jakob.Holkestad.Molnes@gmail.com>
2022-09-13 16:49:54 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    'overrides': [
+        {
+            ig
+        }
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        '@typescript-eslint'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'end-of-file-newline': [
+            'error',
+            'never'
+        ],
+    }
+};
